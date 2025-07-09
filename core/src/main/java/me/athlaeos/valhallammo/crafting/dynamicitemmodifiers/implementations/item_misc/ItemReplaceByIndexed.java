94d02c37e7dddaabe583158a179c542454238c80
Athlaeos <athlaeosbuilds@gmail.com>
2025-07-09 19:26:00 +0200

    moved what should be master branch changes from premium to master

--- core/src/main/java/me/athlaeos/valhallammo/crafting/dynamicitemmodifiers/implementations/item_misc/ItemReplaceByIndexed.java
+++ core/src/main/java/me/athlaeos/valhallammo/crafting/dynamicitemmodifiers/implementations/item_misc/ItemReplaceByIndexed.java
@@ -26,7 +26,10 @@ public class ItemReplaceByIndexed extends DynamicItemModifier implements ResultC
     @Override
     public void processItem(ModifierContext context) {
         ItemStack customItem = CustomItemRegistry.getProcessedItem(item, context);
-        if (ItemUtils.isEmpty(customItem)) return;
+        if (ItemUtils.isEmpty(customItem)) {
+            System.out.println("invalid item");
+            return;
+        } else System.out.println("gonna replace with " + customItem);
         context.getItem().setItem(customItem);
         context.getItem().setMeta(ItemUtils.getItemMeta(customItem));
     }
